fbd97ea95c3a55a627f6861844a744c9246a6ba5
Rubberazer <47650457+Rubberazer@users.noreply.github.com>
2025-11-26 01:17:50 +0100

    initial

--- icd_DO_extractor.py
+++ icd_DO_extractor.py
@@ -55,7 +55,7 @@ for template in templates:
 print('-------Writing variable list--------')
 
 #Opening file
-f = open("variables_extract.csv", "w", encoding="utf-8")
+f = open("variables_extract_DO.csv", "w", encoding="utf-8")
 f.write('NAME' + ',' + 'TYPE' + ',' + 'DESCRIPTION' + '\n')
 
 desc = ''
@@ -115,4 +115,62 @@ for IED in IEDs:
 for column in variables_DO:
     f.write(column[0] + ',' + column[1] + ',' + column[2] +'\n')
 
+f.close()
+
+
+# Find Reports
+print('-------REPORTS NAMES AND COUNT--------')
+
+Variables_report = [['NAME', 'TYPE', 'DESCRIPTION']]
+
+for IED in IEDs:
+    REPORTS = IED.findall('.//{*}ReportControl')
+    for report in REPORTS:
+        report_desc = report.get('desc')
+        report_name = report.get('name')
+        print(report_name + '  ' + verify_if_none(report_desc))
+
+REPORT_count = len(REPORTS)
+print('Report count:' + str(REPORT_count))
+
+# Find Datasets
+print('-------DATASET NAMES AND COUNT--------')
+
+for IED in IEDs:
+    DATASETS = IED.findall('.//{*}DataSet')
+    for dataset in DATASETS:
+        dataset_name = dataset.get('name')
+        dataset_desc = dataset.get('desc')
+        print(dataset_name + '  ' + verify_if_none(dataset_desc))
+
+DATASET_count = len(DATASETS)
+print('Dataset count:' + str(DATASET_count))
+
+# Find FCDAs
+print('------FCDAs---------')
+#Opening file
+f = open("variables_report.csv", "w", encoding="utf-8")
+
+for IED in IEDs:
+    REPORTS = IED.findall('.//{*}ReportControl')
+    for report in REPORTS:
+        DATASETS = IED.findall('.//{*}DataSet')
+        for dataset in DATASETS:
+            if report.get('datSet') == dataset.get('name'):        
+                FCDAs = dataset.findall('./{*}FCDA')
+                for  fcda in FCDAs:
+                    Variables_report.append([IED.get('name') + fcda.get('ldInst') + '/' + fcda.get('prefix')
+                                             + fcda.get('lnClass') + fcda.get('lnInst') + '.' + fcda.get('doName'),'',''])
+                    print(verify_if_none(IED.get('name')) + verify_if_none(fcda.get('ldInst')) + '/' + verify_if_none(fcda.get('prefix'))
+                          + verify_if_none(fcda.get('lnClass')) + verify_if_none(fcda.get('lnInst')) + '.' + verify_if_none(fcda.get('doName')) + ',' + ',')
+
+f.write(Variables_report[0][0] + ',' + Variables_report[0][1] + ',' + Variables_report[0][2] + '\n')
+for R in range(1, len(Variables_report)):       
+    for D in range(len(variables_DO)):
+        if  Variables_report[R][0] == variables_DO[D][0]:
+            Variables_report[R][1] = variables_DO[D][1]
+            Variables_report[R][2] = verify_if_none(variables_DO[D][2])
+    f.write(Variables_report[R][0] + ',' + Variables_report[R][1] + ',' + Variables_report[R][2] + '\n')
+    print(Variables_report[R][0] + ',' + Variables_report[R][1] + ',' + Variables_report[R][2])
+    
 f.close()
